5dff4b00a7a1814a8c4c630004989041b5d9828c
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-12-03 19:00:01 +0700

    some activities by step 2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 // import * as bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as yup from 'yup';
+
+/* eslint no-undef: "error" */
+/* eslint-env browser */
+
+const app = () => {
+  const state = {
+    feeds: [],
+  };
+
+  const rssForm = document.getElementById('rssForm');
+};
+
+app();
