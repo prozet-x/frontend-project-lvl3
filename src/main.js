7d936fbe8be8923aaa6281be06639d828c8f0526
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-12-04 19:05:28 +0700

    some actions by step 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import * as bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as yup from 'yup';
+import onChange from 'on-change';
 
 /* eslint no-undef: "error" */
 /* eslint-env browser */
@@ -10,7 +11,19 @@ const app = () => {
     feeds: [],
   };
 
+  const schema = yup
+    .string()
+    .required('Нужно ввести адрес RSS-ленты!')
+    .url('Ссылка должна быть валидным URL!');
+
   const rssForm = document.getElementById('rssForm');
+  rssForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const rssAdressFromUser = document.getElementById('newRSSAdress').value;
+    schema.validate(rssAdressFromUser)
+    // .then((value) => )
+    .catch((err) => alert(err));
+  });
 };
 
 app();
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
